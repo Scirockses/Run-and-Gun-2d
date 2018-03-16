e5710b7ca2ccf2897358fb41bbec84852e2fd699
Catwolf57 <36024033+Catwolf57@users.noreply.github.com>
2018-03-16 11:25:59 -0600

    Update Screen.java

--- src/game/Screen.java
+++ src/game/Screen.java
@@ -2,4 +2,13 @@ package game;
 
 public class Screen {
 
+  public Screen(){
+		super("Game");
+		this.setSize(1366, 768);
+		this.setResizable(false);
+		this.setUndecorated(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+  
 }
